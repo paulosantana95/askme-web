37e60f9d0bb320b044d46d1b477aa9bfbd7f761a
Paulo Santana <paulosantana.sa@gmail.com>
2024-08-14 21:20:03 -0300

    feat: init askme web front-end and implements aksme api methods

---
+++ src/components/messages.tsx
@@ -0,0 +1,32 @@
+import { useParams } from "react-router-dom";
+import Message from "./message";
+import { getRoomMessages } from "../http/get-room-messages";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+export function Messages() {
+  const { roomId } = useParams()
+
+  if (!roomId) {
+    throw new Error('Messages components must be used within room page')
+  }
+
+  const { data } = useSuspenseQuery({
+    queryKey: ['room-messages', roomId],
+    queryFn: () => getRoomMessages({ roomId })
+  })
+
+  return (
+    <ol className="list-decimal list-outside px-3 space-y-8">
+      {data.messages.map((message) => {
+        return (
+          <Message
+            key={message.id}
+            text={message.text}
+            amountOfReactions={message.amountOfReactions}
+            answered={message.answered}
+          />
+        )
+      })}
+    </ol>
+  )
+}
